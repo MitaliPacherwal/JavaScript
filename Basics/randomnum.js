5759752045105e3d7502d82027de358792f54ea8
Mitali Pacherwal <mitali.pacherwal@robomq.io>
2023-06-01 14:39:41 +0530

    files

--- Basics/randomnum.js
+++ Basics/randomnum.js
@@ -12,7 +12,7 @@ let upper =6
 let lower = 1
 let myRandom = Math.floor(Math.random() * (upper - lower +1)) + lower 
 //+1 here states from 1 to 5 and not 0 to 5 and + lower means floor value of 1 
-console.log(myRandom)
+//console.log(myRandom)
 
 
 //search from array of objects
@@ -21,8 +21,45 @@ const myTodo =["take bath","dance","eat healthy"]
 
 console.log(myTodo.indexOf('take bath'))    //returns index of array element
 
-const newTodo = [{
-    
+const newTodos = [{
+  title:'take bath',
+  isDone:false,  
+}, {
+    title:'dance',
+    isDone:false, 
+},{
+    title:'eat healthy',
+  isDone:true,
 }]
 
+const index= newTodos.findIndex(function(todo, index){
+    console.log(todo);
+    return todo.title === 'dance'
+})
 
+
+console.log(index);
+
+//mthod 1
+const findTodo = function(myTodos ,title){
+const index = myTodos.findIndex(function(todo,index){
+    return todo.title.toLowerCase() === title.toLowerCase()
+})
+return myTodos[index]
+}
+
+let printMe = findTodo(newTodos , 'dance')
+console.log(printMe)
+
+
+//method2
+
+const findTodo2 = function(myTodos ,title){
+    const titleReturned = myTodos.find(function(todo,index){
+        return todo.title.toLowerCase() === title.toLowerCase()
+    })
+    return titleReturned
+    }
+    
+    let printMe2 = findTodo(newTodos , 'dance')
+    console.log(printMe)
